a4dce1d76c7b8b8ee0dde9db86b760400c48a7c0
Lucas Jacinto Gonçalves <42483635+LucasLJG@users.noreply.github.com>
2024-03-15 21:07:49 -0300

    Add files via upload
    Correção da função gera_nome_usuario(nome_usuario) com o retorno do res que estava faltando para printar o nome de um usuário comum ou admin.

--- html_bloco_menu_geral_IMP.py
+++ html_bloco_menu_geral_IMP.py
@@ -50,18 +50,19 @@ def gera_botoes_linha_1_logado(nome_usuario, admin):
   geral, para um usuário que está logado."""
   botoes_sempre = (
       html_elem_button_simples.gera("Minha Conta", 'solicitar_pag_alterar_usuario', None, '#eeeeee'),
-      html_elem_button_simples.gera("Minhas Sessões", 'ver_minhas_sessoes', None, '#eeeeee'),
+      html_elem_button_simples.gera("Minhas Sessões", 'ver_sessoes', None, '#eeeeee'),
       html_elem_button_simples.gera("Sair", 'fazer_logout', None, '#eeeeee'),
       gera_nome_usuario(nome_usuario)
     )
   if admin:
     botoes_sempre += (
-      html_elem_button_simples.gera("Buscar usuários", 'buscar_usuarios', None, '#eeeeee'),
+      html_elem_button_simples.gera("Buscar usuários", 'solicitar_pag_buscar_usuarios', None, '#eeeeee'),
+      html_elem_button_simples.gera("Buscar Videos", 'solicitar_pag_buscar_videos', None, '#eeeeee'),
     )
     botoes_videos = ( )
   else:
     botoes_videos = (
-      html_elem_button_simples.gera("Meus Videos", 'ver_meus_videos', None, '#eeeeee'),
+      html_elem_button_simples.gera("Meus Videos", 'ver_videos', None, '#eeeeee'),
     )
   return botoes_sempre + botoes_videos
 
@@ -79,23 +80,13 @@ def gera_botoes_linha_2():
   geral.  Estes botões são mostrados apenas se o usuário está logado
   e é um administrador."""
 
-  ht_input = html_elem_input.gera(None, "text", "id_objeto", None, None, True, "Id do objeto", None)
-  ht_bt_submit = html_elem_button_submit.gera("Checar Objeto", "ver_objeto", None, '#ffdd22')
-  form_content = \
-    ht_input + \
-    ht_bt_submit
-
-  botoes = (
-    html_elem_button_simples.gera("Acrescentar trecho", "solicitar_pag_acrescentar_trecho", None, '#ffdd22'),
-    html_elem_button_simples.gera("Trafego", "comando_relatorio_de_trafego", None, '#FF00FF'),
-    html_elem_form.gera(form_content)
-
-    # html_elem_button_simples.gera("Alterar trecho", "solicitar_pag_alterar_trecho", None, '#ffdd22'),
-    # html_elem_form_passageiros.gera(),
-    # html_elem_form_buscar_objeto.gera(),
-  )
+  ht_busca_obj_input = html_elem_input.gera(None, "text", "id_objeto", None, None, True, "Id do objeto", None)
+  ht_busca_obj_bt = html_elem_button_submit.gera("Checar Objeto", "ver_objeto", None, '#ffdd22')
+  ht_busca_obj_form = html_elem_form.gera(ht_busca_obj_input + ht_busca_obj_bt)
+  botoes = ( ht_busca_obj_form, )
   return botoes
 
 def gera_nome_usuario(nome_usuario):
   """Gera o texto "Oi {nome}" para o menu geral."""
   res = html_elem_span.gera(None, "Oi " + nome_usuario)
+  return res
