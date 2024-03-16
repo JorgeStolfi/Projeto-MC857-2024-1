a703746ed2a90ef1083df9545772226ec140bc4c
Matheus Sforça <matheussforza@gmail.com>
2024-03-15 21:40:42 -0300

    faz o comando apontar para o html apropriado

--- comando_ver_sessao_IMP.py
+++ comando_ver_sessao_IMP.py
@@ -1,28 +1,19 @@
-import html_bloco_lista_de_sessoes
-import html_pag_generica
-import html_pag_mensagem_de_erro
-import html_bloco_dados_de_sessao
-import html_elem_button_simples
-import html_bloco_erro
+
+import html_pag_ver_sessao
 import obj_sessao
 
 def processa(ses, cmd_args):
   assert ses != None
   assert obj_sessao.aberta(ses)
   usr_ses = obj_sessao.obtem_usuario(ses)
-  if 'id_ses' in cmd_args:
+  erros = list()
+  ses1 = None
+  if 'id_ses' not in cmd_args:
+    erros.append('Chamada sem argumentos')
+  else:
     id_ses = cmd_args['id_ses']
     ses1 = obj_sessao.busca_por_identificador(id_ses)
     if ses1 == None or obj_sessao.obtem_usuario(ses1) != usr_ses:
-      pag = html_bloco_erro.gera('identificador de sessão inválido')
-    
-    else: 
-      ht_conteudo = html_bloco_dados_de_sessao.gera(ses1)
-      args_bt = {'id_ses': id_ses}
-      ht_conteudo += html_elem_button_simples.gera("Fechar sessão", 'fechar_sessao', args_bt, '#FFA700')
-      pag = html_pag_generica.gera(ses, ht_conteudo, None)
-    
-  else:
-    pag = html_bloco_erro.gera('Chamada sem argumentos')
-
-  return pag
+      erros.append('identificador de sessão inválido')
+         
+  return html_pag_ver_sessao.gera(ses, ses1, erros)
--- html_pag_ver_sessao_IMP.py
+++ html_pag_ver_sessao_IMP.py
@@ -1,11 +1,15 @@
-import html_bloco_sessao
 import html_pag_generica
 import html_elem_button_simples
 import html_bloco_dados_de_sessao
+import html_elem_button_submit
 import html_elem_form
 import obj_sessao
 
 def gera(ses, ses1, erros):
+  # Não gera a página de sessão se algum erro for detectado:
+  if (erros != None and len(erros) != 0):
+    return html_pag_generica.gera(ses, '', erros)
+  
   ht_bloco_ses = html_bloco_dados_de_sessao.gera(ses1)
 
   # Somente gera botão caso o usuário da sessao atual seja administrador e a sessão selecionada esteja aberta
@@ -13,11 +17,10 @@ def gera(ses, ses1, erros):
     cmd_args = {}
     cmd_args['id_ses'] = obj_sessao.obtem_identificador(ses1)
 
-    # TODO: escolher cores melhores para os botões
-    fecha_btn = html_elem_button_simples.gera('Fechar sessão', 'fechar_sessao', cmd_args, 'green')
+    fecha_btn = html_elem_button_submit.gera('Fechar sessão', 'fechar_sessao', cmd_args, '#FFA700')
     ht_bloco_ses += fecha_btn
     
   ht_form_ses = html_elem_form.gera(ht_bloco_ses)
 
-  pag = html_pag_generica.gera(ses, ht_bloco_ses, erros)
+  pag = html_pag_generica.gera(ses, ht_form_ses, erros)
   return pag
