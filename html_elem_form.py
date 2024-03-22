8e2855272dee9acecdcb22a0f5d1172f6ac96419
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:53 -0300

    limpeza

--- html_elem_form.py
+++
@@ -1,7 +0,0 @@
-import html_elem_form_IMP
-
-def gera(conteudo):
-  """Retorna um string que é um fragmento HTML consistindo de um <form>...</form>
-  com método "post" e o {conteudo} dado (que deve ter os <input>s necessários).
-  Note que um <form> não pode ocorrer dentro de um parágrafo."""
-  return html_elem_form_IMP.gera(conteudo)
--- html_elem_paragraph_IMP.py
+++
@@ -1,5 +0,0 @@
-
-def gera(estilo, conteudo):
-  est = (" style=\"" + estilo + "\"" if estilo != None and estilo != "" else "")
-  html = "<p" + est + ">" + conteudo + "</p>\n"
-  return html
