8b318a1f13732efd206bafe86e8bd8de90babac0
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:18 -0300

    limpeza

--- html_elem_link_img_IMP.py
+++
@@ -1,7 +0,0 @@
-import html_elem_img
-
-def gera(nome, alt, tam, url):
-  ht_img_crua = html_elem_img.gera(nome, alt, tam)
-  ht_img = "<a href=\"" + url + "\" border=0px>" + ht_img_crua + "</a>"
-  return ht_img
-
