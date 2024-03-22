620d8cb4f5b0eb0181055534302d2770ee72501c
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:39 -0300

    limpeza

--- html_elem_img.py
+++
@@ -1,7 +0,0 @@
-import html_elem_img_IMP
-
-def gera(nome, alt, tam):  
-  """Constrói o HTML para um elemento "<img ...>" 
-  que mostra a imagem "imagens/{nome}" com altura {tam} 
-  e texto alternativo {alt}."""
-  return html_elem_img_IMP.gera(nome, alt, tam)
