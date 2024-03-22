72af84b87d09899abe52f56f001db732c4a44a6b
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-03-22 20:35:08 -0300

    referente a terafa do dia 22/03

---
+++ comando_solicitar_pag_buscar_videos.py
@@ -0,0 +1,15 @@
+import comando_solicitar_pag_buscar_video_IMP
+
+def processa(ses, args):
+    """Esta função é chamada quando o usuário aperta o botão "Buscar Video"
+    no menu geral da pagina do administrador.
+
+    A função retorna uma página HTML {pag} com o formulário que mostra
+    campos que podem ser usados para busca de Videos, com campos editáveis.
+    O formulário deve conter um botão de sumbissão "Buscar".
+
+    O argumento {ses} deve ser uma sessão atualmente aberta.
+
+    A sessão aberta deve ser de um administrador, que pode realizar uma busca
+    para encontrar qualquer video cadastrado"""
+    return comando_solicitar_pag_buscar_video_IMP.processa(ses, args)
---
+++ comando_solicitar_pag_buscar_videos_IMP.py
@@ -0,0 +1,15 @@
+import html_pag_buscar_video
+import html_pag_mensagem_de_erro
+import obj_sessao
+import obj_video
+
+def processa(ses, args):
+  if ses == None:
+    return html_pag_mensagem_de_erro.gera(ses, "Precisa estar logado para usar este comando.")
+  else:
+    usr_ses = obj_sessao.obtem_video(ses)
+    admin = obj_video.obtem_atributo(usr_ses, 'administrador')
+    if not admin:
+       return html_pag_mensagem_de_erro.gera(ses, "Precisa ser administrador para usar este comando.")
+  pag = html_pag_buscar_videos.gera(ses, {}, admin, None)
+  return pag
