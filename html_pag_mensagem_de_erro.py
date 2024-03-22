7149334bf89291849e7d3d295ac41a678c14f346
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:10 -0300

    limpeza

--- html_pag_mensagem_de_erro.py
+++
@@ -1,9 +0,0 @@
-import html_pag_mensagem_de_erro_IMP
-
-def gera(ses, erros):
-  """Retorna uma página de erro com a mensagem de erro {erros}.  O parâmetro
-  {erros} pode ser um string ou uma lista de strings.
-
-  A página terá um botão "OK" que, quando clicado, gera um comando
-  "GET" com URL "principal"."""
-  return html_pag_mensagem_de_erro_IMP.gera(ses, erros)
