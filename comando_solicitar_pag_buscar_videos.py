46cfead2706c9eeffbb51a23fbe3ae59728eb404
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-03-22 20:40:21 -0300

    Update comando_solicitar_pag_buscar_videos.py
    Faltous 2 "s" em videos

--- comando_solicitar_pag_buscar_videos.py
+++ comando_solicitar_pag_buscar_videos.py
@@ -1,4 +1,4 @@
-import comando_solicitar_pag_buscar_video_IMP
+import comando_solicitar_pag_buscar_videos_IMP
 
 def processa(ses, args):
     """Esta função é chamada quando o usuário aperta o botão "Buscar Video"
@@ -12,4 +12,4 @@ def processa(ses, args):
 
     A sessão aberta deve ser de um administrador, que pode realizar uma busca
     para encontrar qualquer video cadastrado"""
-    return comando_solicitar_pag_buscar_video_IMP.processa(ses, args)
+    return comando_solicitar_pag_buscar_videos_IMP.processa(ses, args)
