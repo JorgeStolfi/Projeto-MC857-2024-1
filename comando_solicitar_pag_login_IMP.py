c8e5e72d5048467de1223275c22296fa322596c1
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:49 -0300

    limpeza

--- comando_solicitar_pag_login_IMP.py
+++
@@ -1,8 +0,0 @@
-# Implementação do módulo {comando_solicitar_pag_login}. 
-
-import html_pag_login
-
-def processa(ses, args):
-  pag = html_pag_login.gera(ses, None)
-  return pag
-    
--- html_elem_span_IMP.py
+++
@@ -1,5 +0,0 @@
-def gera(estilo, conteudo):
-  est = (" style=\"" + estilo + "\"" if estilo != None and estilo != "" else "")
-  ctd = (conteudo if conteudo != None else "")
-  html = "<span" + est + ">" + str(ctd) + "</span>"
-  return html
