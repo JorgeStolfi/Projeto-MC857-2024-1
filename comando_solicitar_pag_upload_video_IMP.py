f19770e1c9df5a9b713f3f1e07b8d46e340d4530
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:33 -0300

    limpeza

--- comando_solicitar_pag_upload_video_IMP.py
+++
@@ -1,8 +0,0 @@
-# Implementação do módulo {comando_solicitar_pag_upload_video}. 
-
-import html_pag_upload_video
-
-def processa(ses, cmd_args):
-  pag = html_pag_upload_video.gera(ses, None)
-  return pag
-    
