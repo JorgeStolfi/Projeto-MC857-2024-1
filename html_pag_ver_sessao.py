dd789ccd28c3c9e43e5c3e07a06612429df5991c
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:55 -0300

    limpeza

--- html_pag_ver_sessao.py
+++
@@ -1,6 +0,0 @@
-import html_pag_ver_sessao_IMP
-
-def gera(ses, ses1, erros):
-  """Retorna uma página HTML que mostra os dados da sessão {ses1}
-  (que deve ser um objeto de tipo {obj_sessao.Classe})."""
-  return html_pag_ver_sessao_IMP.gera(ses, ses1, erros)
