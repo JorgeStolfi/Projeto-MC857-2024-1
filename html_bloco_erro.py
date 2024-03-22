556eaba8d76411801c75760c20961b3b8752eea0
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:26 -0300

    limpeza

--- html_bloco_erro.py
+++
@@ -1,8 +0,0 @@
-import html_bloco_erro_IMP
-
-def gera(erros):
-  """O parâmetro {erros} deve ser uma string, que pode incluir quebras 
-  de linha '\n'; ou uma lista ou tupla de tais strings.  Retorna um
-  fragmento de HTML que contém essas mensagens, formatadas como
-  mensagens de erro."""
-  return html_bloco_erro_IMP.gera(erros)
