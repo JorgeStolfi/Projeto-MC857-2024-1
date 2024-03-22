5b6cb38cab39361604e5b5b83bc533e853c713c0
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:35 -0300

    limpeza

--- comando_ver_objeto.py
+++
@@ -1,8 +0,0 @@
-import comando_ver_objeto_IMP
-
-def processa(ses, cmd_args):
-  """Essa função retorna uma página HTML com as informações referentes ao 
-  objeto cujo identificador é {cmd_args['id_objeto']}.  A classe do objeto é
-  determinada pela primeira letra ("U" para {obj_usuario.Classe},  
-  "S" para {obj_sessao.Classe}, etc."""
-  return comando_ver_objeto_IMP.processa(ses, cmd_args)
