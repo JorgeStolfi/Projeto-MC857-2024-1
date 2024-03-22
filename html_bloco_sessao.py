34702bca4b1abe10eeabc840765104e7c7671fc3
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:34:34 -0300

    limpeza

--- html_bloco_sessao.py
+++
@@ -1,7 +0,0 @@
-import html_bloco_sessao_IMP
-
-def gera(ses):
-  """Retorna um fragmento HTML que descreve os dados principais de uma 
-  sessao: identificador da sessao, identificador do usuario
-  e se a sessao está ou não aberta."""
-  return html_bloco_sessao_IMP.gera(ses)
--- html_pag_ver_usuario.py
+++
@@ -1,6 +0,0 @@
-import html_pag_ver_usuario_IMP
-
-def gera(ses, usr, erros):
-  """Retorna uma página HTML que mostra os dados do usuário {usr}
-  (que deve ser um objeto de tipo {obj_usuario.Classe})."""
-  return html_pag_ver_usuario_IMP.gera(ses, usr, erros)
