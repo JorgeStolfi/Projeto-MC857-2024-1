0bba1fd0eca7dce7af1904be2e3bc0029ec1b22f
JorgeStolfi <stolfi@ic.unicamp.br>
2024-03-21 23:54:29 -0300

    limpeza

--- html_pag_mensagem_de_erro_IMP.py
+++
@@ -1,7 +0,0 @@
-import html_pag_generica
-import html_elem_button_simples
-
-def gera(ses, erros):
-  ht_botao = html_elem_button_simples.gera("OK", 'principal', None, '#55ee55')
-  pagina = html_pag_generica.gera(ses, ht_botao, erros)
-  return pagina
