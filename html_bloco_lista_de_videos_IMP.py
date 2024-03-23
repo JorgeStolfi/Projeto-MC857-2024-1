f745bcab1f5107a322a9d91859fb67c210d7b28c
Monica Okamoto Albrecht <ra174928@assis.lab.ic.unicamp.br>
2024-03-22 21:32:10 -0300

    Tarefa da aula 2024-03-22

---
+++ html_bloco_lista_de_videos_IMP.py
@@ -0,0 +1,36 @@
+import obj_video
+import html_bloco_resumo_de_video
+import html_elem_table
+import html_elem_div
+import html_estilo_cabecalho_de_tabela
+import html_bloco_cabecalho
+
+def gera(lista_ids_vid):
+  # Linha de cabeçalho:
+  est_cab = html_estilo_cabecalho_de_tabela.gera()
+  cabs_raw = ['Usuário', 'Arquivo', 'Título', 'Data', 'Duração', 'Largura', 'Altura']
+  cabecalho = [].copy()
+  for cb in cabs_raw:
+    cabecalho.append(html_elem_div.gera(est_cab, cb))
+
+  linhas = [].copy()
+  for id_vid in lista_ids_vid:
+    # busca por identificador do video no banco
+    vid = obj_video.busca_por_identificador(id_vid)
+
+    # Gera uma lista de fragmentos HTML com as informacoes desse video
+    res_campos = html_bloco_resumo_de_video.gera(vid)
+
+    # Adiciona essa lista à lista de linhas para a tabela HTML:
+    linhas.append(res_campos)
+
+  # Gera a tabela HTML a partir da lista de linhas
+  ht_tabela = html_elem_table.gera(linhas, cabecalho)
+
+  ht_titulo = html_bloco_cabecalho.gera("Vídeos", False)
+  ht_conteudo = \
+      ht_titulo + "<br/>\n" + \
+      ht_tabela
+
+  # Devolve a tabela HTML
+  return ht_conteudo
---
+++ testes/html_bloco_lista_de_videos_TST.py
@@ -0,0 +1,41 @@
+import sys
+
+import db_base_sql
+import html_bloco_lista_de_videos
+import db_tabelas
+import util_testes
+
+sys.stderr.write("  Conectando com base de dados...\n")
+res = db_base_sql.conecta("DB", None, None)
+assert res == None
+
+sys.stderr.write("  Criando alguns objetos...\n")
+db_tabelas.cria_todos_os_testes(True)
+
+ids_videos = [
+  "V-00000001",
+  "V-00000002",
+  "V-00000003",
+  "V-00000004",
+  "V-00000005",
+  "V-00000006",
+  "V-00000007",
+  "V-00000008",
+  "V-00000009",
+]
+
+def testa_html_bloco_lista_de_videos(rotulo, *args):
+    """Testa {funcao(*args)}, grava resultado
+    em "testes/saida/{modulo}.{funcao}.{rotulo}.html"."""
+
+    modulo = html_bloco_lista_de_videos   
+    funcao = modulo.gera
+    frag = True     # {True} se for apenas um fragmento HTML, {False} se for página completa.
+    pretty = False  # Se {True}, formata HTML para legibilidate (mas introduz brancos nos textos).
+    util_testes.testa_funcao_que_gera_html(modulo, funcao, rotulo, frag, pretty, *args)
+
+for ver in (False, True):
+  for fechar in (False, True):
+    tag = "ver" + str(ver)[0] + "-fechar" + str(fechar)[0] 
+    testa_html_bloco_lista_de_videos("muitas-" + tag, ids_videos)
+    testa_html_bloco_lista_de_videos("lhufas-" + tag, ids_videos)
