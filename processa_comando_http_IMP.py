bdffd67de607a6c78cd29970ac4e14a5f4fcc540
Joao Victor Soares Aleixo <jovitre@gmail.com>
2024-03-22 21:10:00 -0300

    Implementa o comando ver_videos

--- processa_comando_http_IMP.py
+++ processa_comando_http_IMP.py
@@ -19,6 +19,7 @@ import comando_alterar_usuario
 import comando_ver_objeto
 import comando_ver_sessoes
 import comando_ver_sessao
+import comando_ver_videos
 
 import html_elem_span
 import html_elem_div
@@ -421,9 +422,9 @@ def processa_comando(tipo, ses, dados):
       # Quer formumlário para fazer upload de um video:
       pag = comando_solicitar_pag_upload_video(ses, cmd_args)
 
-    elif cmd == '/ver_meus_videos':
-      # Solicita página com lista de vídeos do usuário:
-      pag = comando_ver_meus_videos.processa(ses, cmd_args)
+    elif cmd == '/ver_videos':
+      # Solicita página com lista de vídeos de algum usuário:
+      pag = comando_ver_videos.processa(ses, cmd_args)
 
     else:
       # Comando não identificado
