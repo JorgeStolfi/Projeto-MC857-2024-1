2b83b8ce04dd91538835966cd717d6c94f4c5e57
Matheus Oliveira <matheusdesouzaoliveira@hotmail.com>
2024-04-05 20:18:06 -0300

    "comando_fazer_upload_video_IMP.processa": Pegar o conteúdo do arquivo e gravar no disco

--- comando_fazer_upload_video.py
+++ comando_fazer_upload_video.py
@@ -4,7 +4,7 @@ def processa(ses, dados):
   """Esta função é chamada quando o usuário (que não deve estar cadastrado) 
   aperta o botão "Upload" no formulário de upload de video.  Ela recebe no
   dicionário {dados} os campos 'arq' e 'titulo' com os dados que o usuário preencheu
-  nesse formulário. Deve processar o upload do arquivo e c criar um novo objeto 
+  nesse formulário. Deve processar o upload do arquivo e criar um novo objeto 
   vídeo com esses dados, registrando na tabela "videos" da base de dados.  A função deve  
   devolver a página de entrada."""
   return comando_fazer_upload_video_IMP.processa(ses, dados)
--- comando_fazer_upload_video_IMP.py
+++ comando_fazer_upload_video_IMP.py
@@ -3,8 +3,11 @@
 # Interfaces do projeto usadas por esta implementação:
 import obj_usuario
 import obj_sessao
+import obj_video
 import html_pag_mensagem_de_erro
 import html_pag_principal
+import db_base_sql
+import datetime
 
 # Outros módulos usados por esta implementação:
 import secrets
@@ -12,5 +15,24 @@ import secrets
 def processa(ses, dados):
   erros = [ "!!! {comando_fazer_upload_video} ainda não foi implementado !!!" ]
   pag = html_pag_mensagem_de_erro.gera(None, erros)
+  
+  # Cria data no formato ISO (aaaa-mm-dd hh:mm:ss fuso).
+  data = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+  dados_do_video = {
+    'usr': ses.usr,             # {obj_usuario.Classe} o usuário que fez upload do vídeo.
+    'arq': dados['arq'],        # {str} nome do arquivo de vídeo.
+    'titulo': dados['titulo'],  # {str} título do vídeo (max 60 caracteres).
+    'data': data,               # data de upload, no formato ISO (aaaa-mm-dd hh:mm:ss fuso).
+    'duracao': 0,               # duração do vídeo em millissegundos.
+    'largura': 0,               # largura de cada frame, em pixels.
+    'altura': 0                 # altura de cada frame, em pixels.
+  }
+
+  # Cria objeto de video para inserir no banco de dados.
+  video = obj_video.cria(dados_do_video)
+
+  # Insere objeto video na tabela `video`.
+  db_base_sql.executa_comando_INSERT('video', video)
 
   return pag
