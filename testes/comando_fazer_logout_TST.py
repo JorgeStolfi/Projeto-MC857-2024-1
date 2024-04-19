4c8af7e5f71a1eefed7e62e5b3670bb846754166
joedsilva18 <joedsilva2016@gmail.com>
2024-04-19 20:46:05 -0300

    comando_fazer_logout - Testes

--- testes/comando_fazer_logout_TST.py
+++ testes/comando_fazer_logout_TST.py
@@ -27,10 +27,20 @@ def testa_comando_fazer_logout(rot_teste, *cmd_args):
   pretty = False # Deve formatar o HTML para facilitar view source?
   util_testes.testa_funcao_que_gera_html(modulo, funcao, rot_teste, frag, pretty, *cmd_args)
 
+
+# Testa logout com uma sessao nao ativa
+ses = None
+testa_comando_fazer_logout("Erro", ses, {})
+assert ses == None
+
+
+# Testa logout com uma sessao ativa
 ses = obj_sessao.busca_por_identificador("S-00000001")
 assert ses != None
 assert obj_sessao.aberta(ses)
 
-testa_comando_fazer_logout("A", ses, {})
+testa_comando_fazer_logout("Ok", ses, {})
+assert not obj_sessao.aberta(ses)
+
 
 sys.stderr.write("Testes terminados normalmente.\n")
