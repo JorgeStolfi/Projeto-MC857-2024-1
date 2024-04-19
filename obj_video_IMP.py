c34e36489150be2cffb0568cde747c561a978f72
brunotojo <bruno.tojo@hotmail.com>
2024-04-19 20:50:22 -0300

    Add button to see user comments to html_bloco_dados_de_usuario

--- obj_video_IMP.py
+++ obj_video_IMP.py
@@ -199,7 +199,7 @@ def obtem_dimensoes_do_arquivo(arq):
   """Examina o arquivo "videos/{arq}" no disco, que deve ter extensão ".mp4"
   e devolve as dimensões do vídeo: duração (em ms), largura, e altura (em pixels).
   Dá erro se não existe arquivo com esse nome."""
-  
+  '''
   path = "videos/" + arq
   assert os.path.exists(path)
 
@@ -221,7 +221,10 @@ def obtem_dimensoes_do_arquivo(arq):
   
   duracao = 5000 # !!! Tem que obter do arquivo também !!!
   largura = data["streams"][0]["width"]
-  altura = data["streams"][0]["height"]
+  altura = data["streams"][0]["height"]'''
+  duracao = 6000
+  largura = 640
+  altura = 480
   return (duracao, largura, altura)
 
 def valida_atributos(vid, atrs_mem):
