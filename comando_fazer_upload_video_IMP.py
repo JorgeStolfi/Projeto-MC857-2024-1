5277a097a6925079197c75140689b9f9dc582fd9
Leonardo Novaes <61755015+LeonardoNoNa@users.noreply.github.com>
2024-04-19 21:25:54 -0300

    Add files via upload

--- comando_fazer_upload_video_IMP.py
+++ comando_fazer_upload_video_IMP.py
@@ -7,9 +7,17 @@ import obj_video
 import html_pag_mensagem_de_erro
 import html_pag_principal
 import html_pag_ver_video
+import db_base_sql
+import datetime
 
 def processa(ses, cmd_args):
+  # Estas condições deveriam valer para comandos gerados
+  # pelas páginas do site:
+  assert ses == None or obj_sessao.aberta(ses), "Sessao inválida"
+  assert type(cmd_args) is dict, "Argumentos inválidos"
+  
   erros = [].copy()
+  
   if ses == None or not obj_sessao.aberta(ses):
     erros.append("Precisa estar logado para executar esta função")
     vid = None
@@ -19,25 +27,30 @@ def processa(ses, cmd_args):
     id_autor = obj_usuario.obtem_identificador(autor)
     
     # Por via das dúvidas:
-    if 'id_usuario' in cmd_args: assert id_autor == cmd_args['id_usuario']
-    if 'id_autor' in cmd_args: assert id_autor == cmd_args['id_autor']
+    if 'usuario' in cmd_args: assert id_autor == cmd_args['usuario']
+    if 'autor' in cmd_args: assert id_autor == cmd_args['autor']
     
     # Nome do arquivo:
-    arq = cmd_args['arq'] if 'arq' in cmd_args else None
     
     # Título:
     titulo = cmd_args['titulo'] if 'titulo' in cmd_args else None
     
+    rd = open('videos/virus.mp4', 'rb')
+    conteudo = rd.read()
+    rd.close()
     # Grava o conteudo do arquivo no disco:
-    wr = open("videos/" + arq, 'wb')
-    # !!! Implementar o upload e gravação no disco !!!
-    wr.close()
-    erros.append("!!! uppload do arquivo ainda não foi implementado !!!")    
+    wr = open("videos/" + 'teste.mp4', 'wb')
+    # !!! Implementar o upload do arquivo {arq} e gravação no disco !!!
+   
     
+    wr.write(conteudo)
+
+    wr.close()
+
     # Registra na tabela de vídeos e cria o objeto:
     atrs = {
       'autor': autor,
-      'arq': arq,
+      'arq' : 'teste.mp4',
       'titulo': titulo,
     }
     vid = obj_video.cria(atrs)
@@ -45,6 +58,7 @@ def processa(ses, cmd_args):
   if vid != None:
     pag = html_pag_ver_video.gera(ses, vid, erros)
   else:
+    # !!! Devia retornar a página de upload !!!
     pag = html_pag_mensagem_de_erro.gera(None, erros)
 
   return pag
--- html_form_upload_video_IMP.py
+++ html_form_upload_video_IMP.py
@@ -1,21 +1,36 @@
-import html_bloco_dados_de_video
+import html_elem_input
 import html_elem_form
 import html_elem_button_simples
 import html_elem_button_submit
+import html_elem_label
+import html_bloco_tabela_de_campos
 
-def gera(id_usr):
+def gera(id_autor, atrs):
   # Constrói tabela com dados:
-  atrs = { 'autor': id_usr } 
-  ht_table = html_bloco_dados_de_video.gera(None, atrs, None, None)
+  atrs = { 'autor': id_autor } 
+  
+  # !!! módulo html_pag_upload_video ainda não terminado !!!"
+  #ht_rotulo = html_elem_label.gera("Arquivo", ":")
 
-  # Constrói formulário com botões 'Entrar' e 'Cancelar':
+  #ht_tabela = html_elem_input.gera("file", 'arq', None, None, None, True, "Escolha", "fazer_upload", True)
+
+  atrs = atrs.copy() # Para não alterar o original.
+  
+  dados_linhas = [].copy()
+
+  dados_linhas.append( ( "Autor",    "text",     'autor',     False,  None,  ) )
+  dados_linhas.append( ( "Titulo",   "textarea", 'titulo',    True,   None,  ) )
+  dados_linhas.append( ( "Arquivo",  "file",     'arquivo',   True,   None,  ) )
+
+  ht_tabela = html_bloco_tabela_de_campos.gera(dados_linhas, atrs)
+
   ht_submit = html_elem_button_submit.gera("Enviar", "fazer_upload_video", None, '#55ee55')
   ht_cancel = html_elem_button_simples.gera("Cancelar", 'pag_principal', None, '#ee5555')
-  conteudo_form = \
-    ( ht_table + "\n" ) + \
+  ht_conteudo = \
+    ( ht_tabela + "\n" ) + \
     ( "    " + ht_submit + "\n" ) + \
     ( "    " + ht_cancel + "\n" )
 
-  ht_form = html_elem_form.gera(conteudo_form)
+  ht_form = html_elem_form.gera(ht_conteudo)
   
   return ht_form
