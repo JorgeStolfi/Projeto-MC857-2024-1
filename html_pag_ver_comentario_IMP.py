57c292eeb584fb9b1deaab5b954c44279401a806
Gabriel Carvalhais <gabrielcarvalhais1@gmail.com>
2024-05-03 20:58:48 -0300

    Botões para comentar vídeos e responder comentários

--- html_pag_ver_comentario_IMP.py
+++ html_pag_ver_comentario_IMP.py
@@ -4,6 +4,7 @@ import html_elem_button_simples
 import html_bloco_dados_de_comentario
 import html_elem_form
 import obj_comentario
+import html_elem_button_submit
 
 def gera(ses, com, erros):
   
@@ -12,7 +13,13 @@ def gera(ses, com, erros):
   edita_texto = False
   ht_bloco_com = html_bloco_dados_de_comentario.gera(id_com, atrs_com, edita_texto)
 
-  ht_form_com = html_elem_form.gera(ht_bloco_com, False)
+  ht_answer = html_elem_button_submit.gera("Responder", "postar_comentario", None, '#55ee55')
+
+  ht_conteudo = \
+        ht_bloco_com + \
+        ht_answer 
+  
+  ht_form_com = html_elem_form.gera(ht_conteudo, False)
 
   pag = html_pag_generica.gera(ses, ht_form_com, erros)
   return pag
