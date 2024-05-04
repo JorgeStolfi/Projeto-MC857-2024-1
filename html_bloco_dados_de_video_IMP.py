8e0b2c1e92f0a9cfded88896aeb4067810fe03d1
Andrey Torres de Lima <ra231442@tolkien.lab.ic.unicamp.br>
2024-05-03 21:15:18 -0300

    231442: 03-05-2024

--- html_bloco_dados_de_video_IMP.py
+++ html_bloco_dados_de_video_IMP.py
@@ -1,15 +1,19 @@
 import obj_usuario
 import obj_video
 import html_elem_paragraph
-import html_bloco_tabela_de_campos
+import html_elem_button_simples
 
 def gera(id_vid, atrs_vid, edita_titulo):
   
   # atrs_tb = ...
   # dados_linhas = ...
   # ht_table = html_bloco_tabela_de_campos.gera(dados_linhas, atrs_tb)
+  
+  # Botão de comentarios
+  ht_comentarios = html_elem_button_simples.gera("Ver comentários", 'html_pag_ver_conversa', None, '#808080')
 
   mensagem = html_elem_paragraph.gera(None, "!!! html_bloco_dados_de_video não implementado ainda !!!")
-  ht_table = mensagem
-
+  ht_table = mensagem + "<br>" + ht_comentarios
+  
+  
   return ht_table
