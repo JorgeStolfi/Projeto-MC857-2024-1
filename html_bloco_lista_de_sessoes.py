f1501d6e01fa5bfe6b36450751221c364686a716
Victor Rigatto <victorrigatto@outlook.com>
2024-05-03 22:38:08 -0300

    implementado parâmetro flag mostrar usuário

--- html_bloco_lista_de_sessoes.py
+++ html_bloco_lista_de_sessoes.py
@@ -1,7 +1,7 @@
 
 import html_bloco_lista_de_sessoes_IMP
 
-def gera(lista_ids_ses, bt_ver, bt_fechar):
+def gera(lista_ids_ses, bt_ver, bt_fechar, mostrar_usr):
   """Retorna um trecho de HTML que descreve as sessoes cujos identificadores
   estão na lista {lista_ids_ses}, juntamente com os botoes de Ver e Fechar."""
-  return html_bloco_lista_de_sessoes_IMP.gera(lista_ids_ses, bt_ver, bt_fechar)
+  return html_bloco_lista_de_sessoes_IMP.gera(lista_ids_ses, bt_ver, bt_fechar, mostrar_usr)
