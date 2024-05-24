63f2e9b513e9e266118796f2c442e03d90ec21e5
BrunoR1907 <brunor1907@gmail.com>
2024-05-24 20:23:00 -0300

    Tarefa 2024-05-24 - RA232450

--- html_pag_buscar_videos_IMP.py
+++ html_pag_buscar_videos_IMP.py
@@ -1,7 +1,12 @@
 import html_pag_generica
+import html_bloco_titulo
 import html_form_buscar_videos
 
 def gera(ses, atrs, erros):
+  ht_titulo = html_bloco_titulo.gera("Dados para a busca de vídeos")
   ht_form = html_form_buscar_videos.gera(atrs)
-  pag = html_pag_generica.gera(ses, ht_form, erros)
+  ht_bloco = \
+    ht_titulo + "<br/>\n" + \
+    ht_form
+  pag = html_pag_generica.gera(ses, ht_bloco, erros)
   return pag
