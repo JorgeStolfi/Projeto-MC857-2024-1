2e6b4380d97982cf4b9e5195cd4c08c25053d668
gustavopurcena <purcena.gustavo@gmail.com>
2024-05-27 11:05:33 -0300

    tarefa 2024-05-24

--- html_form_buscar_videos_IMP.py
+++ html_form_buscar_videos_IMP.py
@@ -4,18 +4,30 @@ import html_elem_button_submit
 import html_elem_button_simples
 import html_bloco_tabela_de_campos
 import html_elem_form
+import obj_usuario
+import obj_sessao
 
-def gera(atrs):
+def gera(atrs, ses):
+
+  if ses != None and obj_sessao.aberta(ses):
+    # Obtem o dono da sessão corrente:
+    ses_dono = obj_sessao.obtem_dono(ses)
+    assert ses_dono != None
+    para_admin = obj_usuario.eh_administrador(ses_dono)
+  else:
+    para_admin = False
 
   # Tupla contendo as seguintes informações: {rot} {tipo} {chave} {editavel} {dica}
   # Para um maior detalhamento, cheque a documentação de `html_bloco_tabela_de_campos.gera`.
   dados_linhas = (
-      ( "ID",       "text",   'video',  True,  "V-NNNNNNNN",      ),
-      ( "Título",   "text",   'titulo', True,  "Bla bla bla",     ),
-      ( "Autor",    "text",   'autor',  True,  "U-NNNNNNNN",   ),
-      ( "Data",     "text",   'data',   True,  "2024",            ),
-      ("Bloqueado", "checkbox", 'bloqueado', True, "", )
-    )
+    ( "ID",       "text",   'video',  True,  "V-NNNNNNNN",      ),
+    ( "Título",   "text",   'titulo', True,  "Bla bla bla",     ),
+    ( "Autor",    "text",   'autor',  True,  "U-NNNNNNNN",   ),
+    ( "Data",     "text",   'data',   True,  "2024",            ),
+  )
+
+  if para_admin:
+    dados_linhas += (("Bloqueado", "checkbox", 'bloqueado', True, ""),)
   # #55ee55
   ht_table = html_bloco_tabela_de_campos.gera(dados_linhas, atrs)
   ht_submit = html_elem_button_submit.gera("Buscar", "buscar_videos", None, None)
--- html_pag_buscar_videos_IMP.py
+++ html_pag_buscar_videos_IMP.py
@@ -2,6 +2,6 @@ import html_pag_generica
 import html_form_buscar_videos
 
 def gera(ses, atrs, erros):
-  ht_form = html_form_buscar_videos.gera(atrs)
+  ht_form = html_form_buscar_videos.gera(atrs, ses)
   pag = html_pag_generica.gera(ses, ht_form, erros)
   return pag
