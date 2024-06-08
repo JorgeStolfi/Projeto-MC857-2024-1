f88700576ee0a1a85b7b41e958697b38e47ec7c8
Andrey Freitas <aelnonym@gmail.com>
2024-06-07 23:13:05 -0300

    feat(botao_upload): aplica estilos ao botao de upload e insere texto com nome do arquivo

--- html_elem_input_IMP.py
+++ html_elem_input_IMP.py
@@ -1,23 +1,61 @@
-
-import html_elem_label
 import html_estilo_button
+import html_estilo_texto
 
 from util_erros import erro_prog, aviso_prog
 
-def input_file_wrapper(ht_input, ident, txt_botao):
-  # Estilo para esconder botao original
-  # Fonte: https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications#using_a_label_element_to_trigger_a_hidden_file_input_element
-  estilo_visualmente_escondido = "<style>" + \
+# Gera um id para os componentes do form de acordo com o ident do pai
+def gerar_id_customizado(ident, id):
+  return str(ident) + "_" + id
+
+def gerar_event_listener(id_alvo, tipo_evento, funcao):
+  return "<script>" + \
+  "document.getElementById(\"" + id_alvo + "\").addEventListener(\"" + tipo_evento + "\", " + funcao + ")" + \
+  "</script>"
+
+# Estilo para esconder botao original
+# Fonte: https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications#using_a_label_element_to_trigger_a_hidden_file_input_element
+def gerar_estilo_visualmente_escondido():
+  return "<style>" + \
   ".visualmente-escondido { position: absolute !important; height: 1px; width: 1px; overflow: hidden; clip: rect(1px, 1px, 1px, 1px); }" + \
   "input.visualmente-escondido:is(:focus, :focus-within) + label { outline: thin dotted; }" + \
   "</style>" 
+
+def input_file_wrapper(ht_input, ident, txt_botao):
+  texto_nenhum_arquivo_selectionado = 'Nenhum arquivo selecionado'
+
+  # IDs usados nos componentes
+  id_botao_nativo = str(ident)
+  id_botao_arquivo = gerar_id_customizado(id_botao_nativo, "botao_arquivo")
+  id_label_botao_arquivo = gerar_id_customizado(id_botao_nativo, "label_botao_arquivo")
+
   estilo_botao = "style=\"" + html_estilo_button.gera("#a091ff") + "\""
+
+  # Gera JS para clique do botão que substitui o nativov
+  funcao_on_click_botao = "function (event) { document.getElementById('"+ id_botao_nativo +"').click() }"
+  script_on_click_botao = gerar_event_listener(id_botao_arquivo, 'click', funcao_on_click_botao)
+
+  botao_interno = "<button id=\"" + id_botao_arquivo + "\" type=\"button\" "+ estilo_botao +"/>" + str(txt_botao) + "</button>"
+
+  # Altera o conteúdo do span de acordo com o nome do arquivo selecionado
+  funcao_on_change_file = "function (event) {" + \
+  "let fileName = event.target.files[0] ? event.target.files[0].name : '"+ texto_nenhum_arquivo_selectionado +"';" + \
+  "document.getElementById('"+ id_label_botao_arquivo +"').textContent = fileName;" + \
+  "}"
+  script_on_change_file = gerar_event_listener(id_botao_nativo, 'change', funcao_on_change_file)
+
+  estilo_label_botao_arquivo = html_estilo_texto.gera("16px", "medium", "#109F4A", None, ( "0px", "0.30em", "0px", "0.70em" ))
+  ht_label_arquivo_selecionado =   "<span id=\""+ id_label_botao_arquivo +"\"" + \
+    "style=\"" + estilo_label_botao_arquivo + "\">" + \
+    texto_nenhum_arquivo_selectionado + \
+    "</span>"
 
   # Junta tudo
   ht_input_wrapper_inicio = "<div>" + \
-  "<label "+ estilo_botao +" for=" + str(ident) + ">" + str(txt_botao) + "</label>"
+    "<label for=" + id_botao_nativo + ">" + botao_interno + "</label>" + \
+    ht_label_arquivo_selecionado
+
   ht_input_wrapper_fim = "</div>"
-  return estilo_visualmente_escondido + ht_input_wrapper_inicio + ht_input + ht_input_wrapper_fim
+  return gerar_estilo_visualmente_escondido() + ht_input_wrapper_inicio + ht_input + ht_input_wrapper_fim + script_on_click_botao + script_on_change_file
 
 def gera(tipo, chave, ident, val_ini, val_min, editavel, dica, cmd, obrigatorio, decimal):
   if tipo == "hidden" and editavel:
@@ -85,6 +123,6 @@ def gera(tipo, chave, ident, val_ini, val_min, editavel, dica, cmd, obrigatorio,
   
   if tipo == "file":
     # Envolve o input com nosso estilo
-    ht_input = input_file_wrapper(ht_input, ident, "Subir video")
+    ht_input = input_file_wrapper(ht_input, ident, "Escolher arquivo")
 
   return ht_texto_simples + ht_input
